ec628736ada8617f1cc425bc26b82b723ea6f6e9
mechztcc <dojathan@gmail.com>
2022-08-01 15:39:53 -0400

    feat: created test to validade when try to create a user with a email in user

--- tests/functional/user/users.spec.ts
+++ tests/functional/user/users.spec.ts
@@ -1,5 +1,16 @@
 import { test } from '@japa/runner'
+import { UserFactory } from 'Database/factories'
 
 test.group('Users users', () => {
-  // Write your test here
+  test('Its should be return 409 when email already in use', async ({ client, assert }) => {
+    const user = await UserFactory.create()
+
+    const anotherUser = { name: 'Usertest', email: user.email, password: '123456' }
+
+    const response = await client.post('/users').json(anotherUser)
+    console.log(response.body())
+    const body = response.body()
+
+    response.assertBodyContains({ code: 'BAD_REQUEST', message: 'E-mail already in use' })
+  })
 })
