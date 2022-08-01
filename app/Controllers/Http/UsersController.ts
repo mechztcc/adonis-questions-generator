c53ad985fad93ebb1a7876ecbee77e1f791e79c6
mechztcc <dojathan@gmail.com>
2022-08-01 13:35:12 -0400

    feat: add create user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import CreateUserValidator from 'App/Validators/CreateUserValidator'
 
 export default class UsersController {
-  public async index({ request, response }: HttpContextContract) {
-    return response.ok({ success: 'true' })
+  public async store({ request, response }: HttpContextContract) {
+    const payload = await request.validate(CreateUserValidator)
+
+    const user = await User.create(payload)
+    return response.created(user)
   }
 }
--- start/users/users.routes.ts
+++ start/users/users.routes.ts
@@ -1,3 +1,3 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/users', 'UsersController.index')
+Route.post('/users', 'UsersController.store')
